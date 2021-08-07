489fd53c8e9a52ee6c43699267e2c3f48a36af11
BenJarrett <b.cl.jarrett@gmail.com>
2021-08-06 22:13:04 -0500

    Class exercise Planets and SpaceSips. Operations and Linq

--- PlanetsAndSpaceships/Program.cs
+++ PlanetsAndSpaceships/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PlanetsAndSpaceships
 {
@@ -6,7 +7,46 @@ namespace PlanetsAndSpaceships
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+
+            // 1. `Add()` Jupiter and Saturn at the end of the list. //
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            Console.WriteLine($"Planet List: { String.Join(',', planetList)}");
+            // END //
+
+            // 2. Create another List that contains that last two planets of our solar system. //
+            List<string> lastTwoPlanets = new List<string>() { "Uranus", "Neptune"};
+            Console.WriteLine($"Solar System List: { String.Join(',', lastTwoPlanets)}");
+            // END //
+
+            // 3. Combine the two lists by using AddRange(). //
+            planetList.AddRange(lastTwoPlanets);
+            Console.WriteLine($"Planet List: { String.Join(',', planetList)}");
+
+            // END //
+
+            // 4. Use Insert() to add Earth, and Venus in the correct order. //
+            planetList.Insert(1, "Earth");
+            planetList.Insert(1, "Venus");
+            Console.WriteLine($"Planet List: { String.Join(',', planetList)}");
+            // END //
+
+            // 5. Use Add() again to add Pluto to the end of the list. //
+            planetList.Add("Pluto");
+            var withPluto = String.Join(',', planetList);
+            Console.WriteLine($"Full Planet List: { String.Join(',', withPluto)}");
+            // END //
+
+            // 6. Now that all the planets are in the list, slice the list using GetRange() in order to extract the rocky planets into a new list called rockyPlanets. //
+            var rockyPlanets = planetList.GetRange(0,4);
+            Console.WriteLine($"Rocky Planets: { String.Join(',', rockyPlanets)}");
+            // END //
+
+            // 7. Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the Remove() method to eliminate it from the end of planetList. //
+            planetList.Remove("Pluto");
+            Console.WriteLine($"Without Pluto: { String.Join(',', planetList)}");
+            // END // 
         }
     }
 }
